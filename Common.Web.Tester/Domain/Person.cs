e0aed8c94f80634bbfe1a34cfbf81288300046c2
Xciles <xciles@gmail.com>
2014-04-27 22:10:18 +0200

    Renaming the Controller! Adding person Domain :)

---
+++ Common.Web.Tester/Domain/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common.Web.Tester.Domain
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string SomeString { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
