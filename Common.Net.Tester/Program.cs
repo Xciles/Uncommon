92ad8501c1723e93a5afedc8e46e526c9781e989
Xciles <xciles@gmail.com>
2014-04-27 22:11:48 +0200

    Adding a will fail

--- Common.Net.Tester/Program.cs
+++ Common.Net.Tester/Program.cs
@@ -23,6 +23,8 @@ namespace Common.Net.Tester
             {
                 var list = await RestRequestHelper.ProcessGetRequest<IList<Person>>(String.Format("{0}/{1}", serviceUrl, "person"), null);
                 var single = await RestRequestHelper.ProcessGetRequest<Person>(String.Format("{0}/{1}", serviceUrl, "person/1"), null);
+
+                var willFail = await RestRequestHelper.ProcessGetRequest<Person>(String.Format("{0}/{1}", serviceUrl, "person"), null);
             }
             catch (RestRequestException ex)
             {
