d7d538444d59de58c124fbd92ca1b7a73573ec35
Xciles <xciles@gmail.com>
2014-05-03 18:28:55 +0200

    Updating AssemblyInfo for NuGet

--- Uncommon/Properties/AssemblyInfo.cs
+++ Uncommon/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Uncommon")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Xciles.Uncommon")]
+[assembly: AssemblyDescription("A common library for PCL containing usefull 'helpers'. This library is still under development!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Uncommon")]
+[assembly: AssemblyProduct("Xciles.Uncommon")]
 [assembly: AssemblyCopyright("Copyright © Hans Harts")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,4 +27,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
