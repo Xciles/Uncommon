6bde1b9d0d58e1aae5f79090baa610a2145ccc14
Xciles <xciles@gmail.com>
2015-02-27 21:13:43 +0100

    Adding interfaces for observable collections

---
+++ Uncommon/Collections/IObservableCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Xciles.Uncommon.Collections
+{
+    public interface IObservableCollection<T> : IList<T>, INotifyCollectionChanged
+    {
+    }
+}
---
+++ Uncommon/Collections/ISortedObservableCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Xciles.Uncommon.Collections
+{
+    public interface ISortedObservableCollection<T> : IObservableCollection<T>, INotifyPropertyChanged
+    {
+        void InsertItem(T item);
+    }
+}
