58d2e6a670c20d14b2b2aa52ea902b15b21fdd33
Xciles <xciles@gmail.com>
2015-02-27 23:43:05 +0100

    Removing old fakes Adding fakes for System.Net.Http Adding quick tests for httpclient Adding Test for httpclient

---
+++ Uncommon.Tests/Net/UncommonHttpClientQuickTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xciles.Uncommon.Tests.Net
+{
+    public class FakeResponseHandler : DelegatingHandler
+    {
+        private readonly Dictionary<Uri, HttpResponseMessage> _fakeResponses = new Dictionary<Uri, HttpResponseMessage>();
+
+        public void AddFakeResponse(Uri uri, HttpResponseMessage responseMessage)
+        {
+            _fakeResponses.Add(uri, responseMessage);
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            if (_fakeResponses.ContainsKey(request.RequestUri))
+            {
+                return _fakeResponses[request.RequestUri];
+            }
+            return new HttpResponseMessage(HttpStatusCode.NotFound) { RequestMessage = request };
+        }
+    }
+
+    [TestClass]
+    public class UncommonHttpClientQuickTests
+    {
+        [TestMethod]
+        public void QuickTests()
+        {
+            QuickTestsAsync().Wait();
+        }
+
+        public async Task QuickTestsAsync()
+        {
+            var fakeResponseHandler = new FakeResponseHandler();
+            fakeResponseHandler.AddFakeResponse(new Uri("http://example.org/test"), new HttpResponseMessage(HttpStatusCode.OK));
+
+            var httpClient = new HttpClient(fakeResponseHandler);
+
+            var response1 = await httpClient.GetAsync("http://example.org/notthere");
+            var response2 = await httpClient.GetAsync("http://example.org/test");
+
+            Assert.AreEqual(response1.StatusCode, HttpStatusCode.NotFound);
+            Assert.AreEqual(response2.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Uncommon.Tests/Net/UncommonHttpClientTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xciles.Uncommon.Tests.Net
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string SomeString { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    [TestClass]
+    public class UncommonHttpClientTests
+    {
+        [TestMethod]
+        public void TestTest()
+        {
+            using (ShimsContext.Create())
+            {
+                var person = new Person
+                {
+                    DateOfBirth = DateTime.Now.Subtract(new TimeSpan(800, 1, 1, 1)),
+                    Firstname = "First",
+                    Lastname = "Person",
+                    PhoneNumber = "0123456789",
+                    SomeString = "This is just a string"
+                };
+
+                ShimHttpClient client = new ShimHttpClient();
+
+
+            }
+        }
+    }
+}
