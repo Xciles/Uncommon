bc7b5efa1291d88dea14caedf04cf9a34c2ef36f
Xciles <xciles@gmail.com>
2015-02-28 23:28:10 +0100

    Addin new base tests

--- Uncommon.Tests/Net/UncommonHttpClientTests.cs
+++ Uncommon.Tests/Net/UncommonHttpClientTests.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Fakes;
+using System.Net.Http;
 using System.Net.Http.Fakes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Global.Fakes;
 using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Xciles.Uncommon.Net;
 
 namespace Xciles.Uncommon.Tests.Net
 {
@@ -20,6 +30,11 @@ namespace Xciles.Uncommon.Tests.Net
         [TestMethod]
         public void TestTest()
         {
+            GetAsyncTestAsync().Wait();
+        }
+
+        public async Task GetAsyncTestAsync()
+        {
             using (ShimsContext.Create())
             {
                 var person = new Person
@@ -32,8 +47,80 @@ namespace Xciles.Uncommon.Tests.Net
                 };
 
                 ShimHttpClient client = new ShimHttpClient();
+                client.GetAsyncUriHttpCompletionOptionCancellationToken = (uri, option, arg3) =>
+                {
+                    return Task.FromResult(new HttpResponseMessage()
+                    {
+                        Content = new StringContent(JsonConvert.SerializeObject(person)),
+                        StatusCode = HttpStatusCode.OK
+                    });
+                };
+                client.GetAsyncUri = uri =>
+                {
+                    return Task.FromResult(new HttpResponseMessage());
+                };
 
+                client.GetAsyncUriCancellationToken = (uri, token) =>
+                {
+                    return Task.FromResult(new HttpResponseMessage());
+                };
 
+                client.GetAsyncUriHttpCompletionOption = (uri, option) =>
+                {
+                    return Task.FromResult(new HttpResponseMessage());
+                };
+
+                ShimHttpClient.AllInstances.GetAsyncUriHttpCompletionOptionCancellationToken = (httpClient, uri, arg3, arg4) => 
+                {
+                    //return ShimsContext.ExecuteWithoutShims(() => httpClient.GetAsync(uri, arg3, arg4));
+                    return Task.FromResult(new HttpResponseMessage()
+                    {
+                        Content = new StringContent(JsonConvert.SerializeObject(person)),
+                        StatusCode = HttpStatusCode.OK
+                    });
+                };
+
+                ShimAsyncExtensions.GetResponseAsyncWebRequest = request =>
+                {
+                    return ShimsContext.ExecuteWithoutShims(() => request.GetResponseAsync());
+                };
+
+                //ShimHttpWebRequest.AllInstances.BeginGetResponseAsyncCallbackObject = (request, callback, arg3) =>
+                //{
+                //    return ShimsContext.ExecuteWithoutShims(() => request.BeginGetResponse(callback, arg3));
+                //};
+
+                //ShimHttpWebResponse.AllInstances.StatusCodeGet = response =>
+                //{
+                //    return ShimsContext.ExecuteWithoutShims(() => response.StatusCode);
+                //};
+
+                //ShimHttpWebResponse.AllInstances.GetResponseStream = response =>
+                //{
+                //    var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(person));
+
+                //    var stream = new MemoryStream(bytes);
+
+                //    return stream;
+                //};
+
+                //ShimHttpWebResponse.AllInstances.HeadersGet = response =>
+                //{
+                //    //return ShimsContext.ExecuteWithoutShims(() => response.Headers);
+
+                //    var headers = new WebHeaderCollection();
+                //    headers.Add(HttpRequestHeader.ContentType, "text/html; charset=utf-8");
+
+                //    return headers;
+                //    //base = {Content-Encoding: gzip Content-Type: text/html; charset=utf-8 Content-Length: 155}
+                //};
+
+                HttpClient aa = new HttpClient();
+                var a = await aa.GetAsync(new Uri("http://www.xciles.com/"), HttpCompletionOption.ResponseContentRead, CancellationToken.None);
+
+                UncommonHttpClient bla = new UncommonHttpClient();
+
+                var t = await bla.GetJsonAsync<Person>("http://www.xciles.com/");
             }
         }
     }
