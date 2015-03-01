eeea9d17d5627650755f18a56eb0c5383d6d1ecc
Xciles <xciles@gmail.com>
2015-03-01 15:11:43 +0100

    Adding gzip Send Tests Changes to the "quick" tests

--- Uncommon.Tests/Net/UncommonHttpClientGzipTest.cs
+++ Uncommon.Tests/Net/UncommonHttpClientGzipTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Net.Fakes;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Xciles.Uncommon.Handler;
 using Xciles.Uncommon.Net;
 
 namespace Xciles.Uncommon.Tests.Net
@@ -15,12 +17,12 @@ namespace Xciles.Uncommon.Tests.Net
     public class UncommonHttpClientGzipTests
     {
         [TestMethod]
-        public void GetJsonAsyncGzipTest()
+        public void GetJsonAsyncNoGzipTest()
         {
-            GetJsonAsyncGzipTestAsync().Wait();
+            GetJsonAsyncNoGzipTestAsync().Wait();
         }
 
-        public async Task GetJsonAsyncGzipTestAsync()
+        public async Task GetJsonAsyncNoGzipTestAsync()
         {
             using (ShimsContext.Create())
             {
@@ -59,6 +61,65 @@ namespace Xciles.Uncommon.Tests.Net
 
                     //base = {Content-Encoding: gzip Content-Type: text/html; charset=utf-8 Content-Length: 155}
 
+                    return headers;
+                };
+
+                using (var client = new UncommonHttpClient())
+                {
+                    var result = await client.GetJsonAsync<Person>("http://www.xciles.com/");
+
+                    Assert.AreEqual(person.Firstname, result.Firstname);
+                    Assert.AreEqual(person.Lastname, result.Lastname);
+                    Assert.AreEqual(person.PhoneNumber, result.PhoneNumber);
+                }
+            }
+        }
+        
+        [TestMethod]
+        public void GetJsonAsyncGzipTest()
+        {
+            GetJsonAsyncGzipTestAsync().Wait();
+        }
+
+        public async Task GetJsonAsyncGzipTestAsync()
+        {
+            using (ShimsContext.Create())
+            {
+                var person = new Person
+                {
+                    DateOfBirth = DateTime.Now.Subtract(new TimeSpan(800, 1, 1, 1)),
+                    Firstname = "First",
+                    Lastname = "Person",
+                    PhoneNumber = "0123456789",
+                    SomeString = "This is just a string"
+                };
+
+                ShimHttpWebRequest.AllInstances.BeginGetResponseAsyncCallbackObject = (request, callback, arg3) =>
+                {
+                    return ShimsContext.ExecuteWithoutShims(() => request.BeginGetResponse(callback, arg3));
+                };
+
+                ShimHttpWebResponse.AllInstances.StatusCodeGet = response =>
+                {
+                    return ShimsContext.ExecuteWithoutShims(() => response.StatusCode);
+                };
+
+                ShimHttpWebResponse.AllInstances.GetResponseStream = response =>
+                {
+                    var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(person));
+
+                    var stream = new MemoryStream(bytes);
+
+                    var s = new GZipStream(stream, CompressionMode.Compress);
+
+                    return s;
+                };
+
+                ShimHttpWebResponse.AllInstances.HeadersGet = response =>
+                {
+                    var headers = new WebHeaderCollection();
+                    headers.Add(HttpRequestHeader.ContentType, "text/html; charset=utf-8");
+                    headers.Add(HttpRequestHeader.ContentEncoding, "gzip");
                     return headers;
                 };
 
--- Uncommon.Tests/Net/UncommonHttpClientQuickTests.cs
+++ Uncommon.Tests/Net/UncommonHttpClientQuickTests.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xciles.Uncommon.Net;
 
 namespace Xciles.Uncommon.Tests.Net
 {
@@ -40,13 +41,35 @@ namespace Xciles.Uncommon.Tests.Net
             var fakeResponseHandler = new FakeResponseHandler();
             fakeResponseHandler.AddFakeResponse(new Uri("http://example.org/test"), new HttpResponseMessage(HttpStatusCode.OK));
 
-            var httpClient = new HttpClient(fakeResponseHandler);
+            using (var client = new UncommonHttpClient(fakeResponseHandler))
+            {
+                var response1 = await client.GetAsync("http://example.org/notthere");
+                var response2 = await client.GetAsync("http://example.org/test");
 
-            var response1 = await httpClient.GetAsync("http://example.org/notthere");
-            var response2 = await httpClient.GetAsync("http://example.org/test");
+                Assert.AreEqual(response1.StatusCode, HttpStatusCode.NotFound);
+                Assert.AreEqual(response2.StatusCode, HttpStatusCode.OK);
+            }
+        }
 
-            Assert.AreEqual(response1.StatusCode, HttpStatusCode.NotFound);
-            Assert.AreEqual(response2.StatusCode, HttpStatusCode.OK);
+        [TestMethod]
+        public void QuickDisposeTests()
+        {
+            QuickDisposeTestsAsync().Wait();
+        }
+
+        public async Task QuickDisposeTestsAsync()
+        {
+            var fakeResponseHandler = new FakeResponseHandler();
+            fakeResponseHandler.AddFakeResponse(new Uri("http://example.org/test"), new HttpResponseMessage(HttpStatusCode.OK));
+
+            using (var client = new UncommonHttpClient(fakeResponseHandler, false))
+            {
+                var response1 = await client.GetAsync("http://example.org/notthere");
+                var response2 = await client.GetAsync("http://example.org/test");
+
+                Assert.AreEqual(response1.StatusCode, HttpStatusCode.NotFound);
+                Assert.AreEqual(response2.StatusCode, HttpStatusCode.OK);
+            }
         }
     }
 }
