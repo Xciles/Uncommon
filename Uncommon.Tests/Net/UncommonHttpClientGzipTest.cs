b13090ca758d46fa5102b3d49c8895632809631a
Xciles <xciles@gmail.com>
2015-03-01 16:13:51 +0100

    Removing gzip things Adding the base httpclient decompression

--- Uncommon.Tests/Net/UncommonHttpClientGzipTest.cs
+++ Uncommon.Tests/Net/UncommonHttpClientGzipTest.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Net.Fakes;
+using System.Net.Http;
+using System.Net.Http.Fakes;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.QualityTools.Testing.Fakes;
@@ -61,65 +64,6 @@ namespace Xciles.Uncommon.Tests.Net
 
                     //base = {Content-Encoding: gzip Content-Type: text/html; charset=utf-8 Content-Length: 155}
 
-                    return headers;
-                };
-
-                using (var client = new UncommonHttpClient())
-                {
-                    var result = await client.GetJsonAsync<Person>("http://www.xciles.com/");
-
-                    Assert.AreEqual(person.Firstname, result.Firstname);
-                    Assert.AreEqual(person.Lastname, result.Lastname);
-                    Assert.AreEqual(person.PhoneNumber, result.PhoneNumber);
-                }
-            }
-        }
-        
-        [TestMethod]
-        public void GetJsonAsyncGzipTest()
-        {
-            GetJsonAsyncGzipTestAsync().Wait();
-        }
-
-        public async Task GetJsonAsyncGzipTestAsync()
-        {
-            using (ShimsContext.Create())
-            {
-                var person = new Person
-                {
-                    DateOfBirth = DateTime.Now.Subtract(new TimeSpan(800, 1, 1, 1)),
-                    Firstname = "First",
-                    Lastname = "Person",
-                    PhoneNumber = "0123456789",
-                    SomeString = "This is just a string"
-                };
-
-                ShimHttpWebRequest.AllInstances.BeginGetResponseAsyncCallbackObject = (request, callback, arg3) =>
-                {
-                    return ShimsContext.ExecuteWithoutShims(() => request.BeginGetResponse(callback, arg3));
-                };
-
-                ShimHttpWebResponse.AllInstances.StatusCodeGet = response =>
-                {
-                    return ShimsContext.ExecuteWithoutShims(() => response.StatusCode);
-                };
-
-                ShimHttpWebResponse.AllInstances.GetResponseStream = response =>
-                {
-                    var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(person));
-
-                    var stream = new MemoryStream(bytes);
-
-                    var s = new GZipStream(stream, CompressionMode.Compress);
-
-                    return s;
-                };
-
-                ShimHttpWebResponse.AllInstances.HeadersGet = response =>
-                {
-                    var headers = new WebHeaderCollection();
-                    headers.Add(HttpRequestHeader.ContentType, "text/html; charset=utf-8");
-                    headers.Add(HttpRequestHeader.ContentEncoding, "gzip");
                     return headers;
                 };
 
--- Uncommon/Handler/UncommonHttpClientHandler.cs
+++ Uncommon/Handler/UncommonHttpClientHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,24 +8,20 @@ namespace Xciles.Uncommon.Handler
 {
     public class UncommonHttpClientHandler : HttpClientHandler
     {
-        public override bool SupportsAutomaticDecompression
+        public UncommonHttpClientHandler()
         {
-            get { return true; }
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
         }
+
+        //public override bool SupportsAutomaticDecompression
+        //{
+        //    get { return true; }
+        //}
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
-
-            // Todo add gzip when sending.
-            var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-
-            if (response.Content.Headers.ContentEncoding.Contains("gzip"))
-            {
-                var content = new GZipHttpContent(await response.Content.ReadAsStreamAsync().ConfigureAwait(false), response.Content.Headers);
-                response.Content = content;
-            }
-            return response;
+            // todo add gzip compression when sending
+            return await base.SendAsync(request, cancellationToken);
         }
     }
 }
