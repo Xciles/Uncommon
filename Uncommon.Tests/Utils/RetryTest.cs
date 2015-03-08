7e3d11d1daac275ac7cf76fba1f251f6fb5ebea8
Xciles <xciles@gmail.com>
2015-03-08 21:15:51 +0100

    Adding empty RetryTestC Class...

---
+++ Uncommon.Tests/Utils/RetryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xciles.Uncommon.Tests.Utils
+{
+    [TestClass]
+    public class RetryTest
+    {
+        [TestMethod]
+        public void DoTest()
+        {
+            
+        }
+    }
+}
