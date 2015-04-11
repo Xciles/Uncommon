f670e95eeb7df8a56d18197ace410b0fa0401549
Xciles <Xciles@gmail.com>
2015-04-11 10:05:40 +0200

    Adding the new Exception response logic to the UncommonRequestException Marked 2 properties Obsolete since they will no longer be used as a direct result of the above change

--- Uncommon/Net/UncommonRequestException.cs
+++ Uncommon/Net/UncommonRequestException.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace Xciles.Uncommon.Net
 {
     public class UncommonRequestException : Exception
     {
         public EUncommonRequestExceptionStatus RequestExceptionStatus { get; set; }
+        [Obsolete ("What was in here has been moved to innerexception")]
         public Exception Exception { get; set; }
+        [Obsolete("This is no longer being used. Please use ConvertExceptionResponseToObject<T> for parsing to a <T> of your choise.")]
         public ServiceExceptionResult ServiceExceptionResult { get; set; }
         public string Information { get; set; }
         public WebExceptionStatus WebExceptionStatus { get; set; }
         public HttpStatusCode StatusCode { get; set; }
+
+        internal string ExceptionResponseAsString { get; set; }
+
+        public T ConvertExceptionResponseToObject<T>()
+        {
+            var jsonSerializerSettings = new JsonSerializerSettings() { PreserveReferencesHandling = PreserveReferencesHandling.Objects };
+
+            return JsonConvert.DeserializeObject<T>(ExceptionResponseAsString, jsonSerializerSettings);
+        }
     }
 }
