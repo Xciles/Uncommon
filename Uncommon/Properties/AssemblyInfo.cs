ae9caa59800915f9770c013faf00aee77700c4ed
Xciles <xciles@gmail.com>
2016-08-04 23:01:04 +0200

    Moving project to xproj Adding netstandard1.1 and PCL111 Bumping version to 1.9.1-beta

--- Uncommon/Properties/AssemblyInfo.cs
+++ Uncommon/Properties/AssemblyInfo.cs
@@ -1,5 +1,11 @@
 ﻿using System.Resources;
 using System.Reflection;
+#if _NETCORE_
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endif
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -24,4 +30,16 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.9.1.0")]
+
+#if _NETCORE_
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25da66a5-9ba4-4129-a6b0-ec8989eddef9")]
+
+#endif
